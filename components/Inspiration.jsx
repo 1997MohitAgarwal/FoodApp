038033d99cadd7bddd094e9356a5baedcb987be1
1997MohitAgarwal <152138497+1997MohitAgarwal@users.noreply.github.com>
2024-10-12 18:23:13 +0530

    fixes

--- components/Inspiration.jsx
+++ components/Inspiration.jsx
@@ -5,17 +5,17 @@ export default function Inspiration(){
     <div className="p-4" >
      <p className="text-center text-3xl text-gray-700 textneww mb-4">Looking for Inspiration?</p>
     <div className="grid grid-cols-1 sm:grid-cols-3 gap-5" >
-       <a href="https://realfood.tesco.com/recipes/bacon-and-mushroom-breakfast-hash.html" target="_blank">
+       <a href="https://realfood.tesco.com/recipes/bacon-and-mushroom-breakfast-hash.html" target="_blank" rel="noreferrer">
       <div className="cursor-pointer">
       <img src="/bacon1.png"></img>
         <p className="text-center text-xl font-semibold mt-1 bg-green-500 py-2 marginset text-white">Bacon and mushroom hash</p>
       </div></a>
-       <a href="https://realfood.tesco.com/recipes/bacon-and-spinach-spaghetti.html" target="_blank">
+       <a href="https://realfood.tesco.com/recipes/bacon-and-spinach-spaghetti.html" target="_blank" rel="noreferrer">
       <div className="cursor-pointer">
       <img src="/bacon2.png"></img>
         <p className="text-center text-xl font-semibold mt-1 bg-green-500 py-2 marginset text-white">Bacon and spinach spaghetti</p>
       </div></a>
-      <a href="https://realfood.tesco.com/recipes/bacon-and-tomato-muffins.html" target="_blank">
+      <a href="https://realfood.tesco.com/recipes/bacon-and-tomato-muffins.html" target="_blank" rel="noreferrer">
         <div className="cursor-pointer">
       <img src="/bacon3.png"></img>
       <p className="text-center text-xl font-semibold mt-1 bg-green-500 py-2 marginset text-white">Bacon and tomato muffin</p>
